43fb87aaa90d7565efe8f84d9247e6f348bfb893
Abhi Yerra <abhi@berkeley.edu>
2022-04-19 15:12:19 -0700

    Test

--- nginx.tf
+++ nginx.tf
@@ -20,3 +20,26 @@ resource "helm_release" "nginx" {
     helm_release.prometheus,
   ]
 }
+
+
+resource "helm_release" "nginx2" {
+  name             = "nginx2"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "nginx"
+  create_namespace = true
+
+  values = [
+    var.nginx_yml_file == null ? "${file("${path.module}/nginx.yml")}" : "${var.nginx_yml_file}"
+  ]
+
+  set {
+    name  = "controller.replicaCount"
+    value = var.nginx_replica_count
+  }
+
+  depends_on = [
+    helm_release.cert-manager,
+    helm_release.prometheus,
+  ]
+}
