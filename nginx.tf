e6a8132ba78e999a6ae7b9f122fc574b8c18126f
Abhi Yerra <abhi@berkeley.edu>
2022-04-19 15:26:45 -0700

    Added nginx

--- nginx.tf
+++ nginx.tf
@@ -6,29 +6,6 @@ resource "helm_release" "nginx" {
   namespace        = "nginx"
   create_namespace = true
 
-  # values = [
-  #   var.nginx_yml_file == null ? "${file("${path.module}/nginx.yml")}" : "${var.nginx_yml_file}"
-  # ]
-
-  set {
-    name  = "controller.replicaCount"
-    value = var.nginx_replica_count
-  }
-
-  depends_on = [
-    helm_release.cert-manager,
-    helm_release.prometheus,
-  ]
-}
-
-
-resource "helm_release" "nginx2" {
-  name             = "nginx2"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  namespace        = "nginx2"
-  create_namespace = true
-
   values = [
     var.nginx_yml_file == null ? "${file("${path.module}/nginx.yml")}" : "${var.nginx_yml_file}"
   ]
