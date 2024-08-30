c2e60652b801079723a2415af6aabb61651cc597
Abhi Yerra <abhi@opszero.com>
2024-08-30 07:45:16 -0700

    Disable

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "grafana_admin_password" {
-  value     = var.grafana_admin_password != "" ? var.grafana_admin_password : random_password.grafana_admin_password[0].result
+  value     = try(var.grafana_admin_password != "" ? var.grafana_admin_password : random_password.grafana_admin_password[0].result, "")
   sensitive = true
 }
