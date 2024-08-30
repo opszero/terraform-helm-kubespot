d050f34c703101f4500856f466306aa813f2a4e0
Sohan Yadav <sohanyadavdevops@gmail.com>
2024-08-30 17:35:04 +0530

    update resource

--- loki.tf
+++ loki.tf
@@ -1,6 +1,18 @@
-resource "helm_release" "grafana" {
-  count = var.grafana_enabled && var.grafana_loki_enabled ? 1 : 0
+resource "random_password" "s3_loki_name" {
+  count   = var.grafana_enabled && var.grafana_loki_enabled ? 1 : 0
+  length  = 16
+  special = false
+  upper   = false # Disable uppercase letters
+}
 
+# Create an S3 bucket
+resource "aws_s3_bucket" "s3_loki" {
+  bucket = "loki-${random_password.s3_loki_name[0].result}"
+}
+
+
+resource "helm_release" "loki" {
+  count            = var.grafana_enabled && var.grafana_loki_enabled ? 1 : 0
   chart            = "loki"
   name             = "loki"
   namespace        = "loki"
@@ -8,13 +20,12 @@ resource "helm_release" "grafana" {
   repository       = "https://grafana.github.io/helm-charts"
   version          = "6.10.2"
 
-
   values = [
-      var.loki_yml_file == null ? file("${path.module}/loki.yml") : "${var.loki_yml_file}"
+    var.loki_yml_file != null ? var.loki_yml_file : templatefile("${path.module}/loki.yml", {
+    s3_bucket = aws_s3_bucket.s3_loki.bucket })
   ]
+
 }
-
-
 
 
 
--- variables.tf
+++ variables.tf
@@ -143,20 +143,20 @@ variable "grafana_datasources" {
     isDefault = bool
   }))
   default = [
-#     {
-#       name      = "Postgres"
-#       type      = "postgres"
-#       url       = "postgresql://user:password@postgres-server.database.svc.cluster.local:5432/dbname"
-#       access    = "proxy"
-#       isDefault = false
-#     },
-#     {
-#       name      = "Loki"
-#       type      = "loki"
-#       url       = "http://loki-server.loki.svc.cluster.local"
-#       access    = "proxy"
-#       isDefault = false # This should be false
-#     }
+    #     {
+    #       name      = "Postgres"
+    #       type      = "postgres"
+    #       url       = "postgresql://user:password@postgres-server.database.svc.cluster.local:5432/dbname"
+    #       access    = "proxy"
+    #       isDefault = false
+    #     },
+    #     {
+    #       name      = "Loki"
+    #       type      = "loki"
+    #       url       = "http://loki-server.loki.svc.cluster.local"
+    #       access    = "proxy"
+    #       isDefault = false # This should be false
+    #     }
   ]
 }
 
