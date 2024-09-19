29f9d9fec8aa06ed5ee16bd6ccc2788f1335f578
Sohan Yadav <sohanyadavdevops@gmail.com>
2024-09-19 14:59:44 +0530

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
 output "grafana_admin_password" {
-  value     = var.grafana_admin_password != "" ? var.grafana_admin_password : random_password.grafana_admin_password[0].result
+  value = var.grafana_admin_password != "" ? var.grafana_admin_password : (
+    var.grafana_enabled && var.grafana_admin_password == "" && length(random_password.grafana_admin_password) > 0
+    ? random_password.grafana_admin_password[0].result
+    : ""
+  )
   sensitive = true
 }
