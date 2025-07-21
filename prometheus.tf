3ceb4586bd114665835e4ac34dd136844667b9cf
sohanyadav <sohanyadavdevops@gmail.com>
2025-07-21 14:41:11 +0530

    fix helm issue

--- prometheus.tf
+++ prometheus.tf
@@ -14,46 +14,67 @@ resource "helm_release" "prometheus" {
     }),
   ] : []
 
-  set = concat(
-    [
-      {
-        name  = "podSecurityPolicy.enabled"
-        value = "true"
-      },
-      {
-        name  = "server.retention"
-        value = "1d"
-      },
-      {
-        name  = "server.persistentVolume.enabled"
-        value = tostring(var.prometheus_persistence_storage)
-      },
-      {
-        name  = "server.persistentVolume.storageClass"
-        value = var.storage_class
-      },
-      {
-        name  = "alertmanager.persistence.enabled"
-        value = "false"
-      }
-    ],
+  set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
 
-    length(var.pushgateway_ingress_host) > 0 ? [{
-      name  = "values"
-      value = templatefile("${path.module}/prometheus.yml", {
-        PUSH_GATEWAY_INGRESS_HOSTS = var.pushgateway_ingress_host
-      })
-    }] : [],
+  set {
+    name  = "server.retention"
+    value = "1d"
+  }
 
-    var.prometheus_persistence_storage != false ? [
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = var.prometheus_persistence_storage
+  }
+
+  set {
+    name  = "server.persistentVolume.storageClass"
+    value = var.storage_class
+  }
+
+  dynamic "set" {
+    for_each = length(var.pushgateway_ingress_host) > 0 ? [
       {
-        name  = "server.persistentVolume.existingClaim"
-        value = ""
-      },
-      {
-        name  = "server.persistentVolume.size"
-        value = "8Gi"
+        name = "values"
+        value = templatefile("${path.module}/prometheus.yml", {
+          PUSH_GATEWAY_INGRESS_HOSTS = var.pushgateway_ingress_host
+        })
       }
     ] : []
-  )
+
+    content {
+      name  = set.value.name
+      value = set.value.value
+    }
+  }
+
+
+  ## Prometheus server data Persistent Volume existing claim name
+  ## Requires server.persistentVolume.enabled: true
+  ## If defined, PVC must be created manually before volume will be bound
+  ## Example if we want to use EFS, create storage class and pvc and add the
+  ## claim name here
+  dynamic "set" {
+    for_each = var.prometheus_persistence_storage != false ? [1] : []
+    content {
+      name  = "server.persistentVolume.existingClaim"
+      value = ""
+    }
+  }
+
+  dynamic "set" {
+    for_each = var.prometheus_persistence_storage != false ? [1] : []
+    content {
+      name  = "server.persistentVolume.size"
+      value = "8Gi"
+    }
+  }
+
+  set {
+    name  = "alertmanager.persistence.enabled"
+    value = false
+  }
+
 }
